b23b1cd03434f2ed8f71e06c4993f6cdba7a82ee
Asfand-Yar-dev <asfandyar273263@gmail.com>
2026-01-12 17:04:32 +0500

    Refactor functions to accept a callable parameter for enhanced flexibility

--- Task-1.py
+++ Task-1.py
@@ -1,12 +1,14 @@
-def func1(x, y):
+# TODO
+#extra func 1 parameter that passed another function
+# funct 1 pass the parameter that execute another function
+# function 2 and function 3 that passed through function 1 with new parameters
+
+def function_1(x,y,func):
+    return (func(x,y))
+def add(x,y):
     return x + y
+def multiply(x,y):
+    return x * y
 
-def func2(a, b):
-    result = func1(a, b)
-    return result * 2
-
-def func3(p, q):
-    result = func2(p, q)
-    return result - 5
-
-print(func3(2, 3))
+print(function_1(10,5,add))
+print(function_1(10,5,multiply))
